f54487dbcbe86db501194dbf830ef81c006b206a
DESKTOP-UR59BPE\admin <gametac>
2025-11-12 09:08:19 +0700

    - Bài 1-4

---
+++ Assets/Scripts/DropDragCtrl.cs
@@ -0,0 +1,108 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropDragCtrl : MonoBehaviour
+{
+    [SerializeField] private Image _imgFoodDrag;
+
+    private FoodSlot _currentFood, _cacheFood;
+    private bool _hasDrag;
+    private Vector3 _offset;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // check khi click chuot
+        {
+            _currentFood = Utils.GetRayCastUI<FoodSlot>(Input.mousePosition); // check o vi tri click chuot xem co Ui gan class FoodSlot
+            if(_currentFood != null && _currentFood.HasFood)
+            {
+                _hasDrag = true;
+                _cacheFood = _currentFood;
+                // Gan sprite food cho dummy image
+                _imgFoodDrag.gameObject.SetActive(true);
+                _imgFoodDrag.sprite = _currentFood.GetSpriteFood;
+                _imgFoodDrag.SetNativeSize();
+                _imgFoodDrag.transform.position = _currentFood.transform.position; // gan vi tri
+
+
+                //tinh offset
+                Vector3 mouseWordPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                _offset = mouseWordPos - _currentFood.transform.position;
+
+                _currentFood.OnActiveFood(false);
+            }
+        }
+
+        if (_hasDrag)
+        {
+            Vector3 mouseWordPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 foodPos = mouseWordPos + _offset;
+            foodPos.z = 0f;
+            _imgFoodDrag.transform.position = foodPos;
+
+            FoodSlot slot = Utils.GetRayCastUI<FoodSlot>(Input.mousePosition);
+            if(slot != null  )
+            {
+                if (!slot.HasFood) // vi tri item chua co food
+                {
+                    if (_cacheFood == null || _cacheFood.GetInstanceID() != slot.GetInstanceID())
+                    {
+                        _cacheFood?.OnHideFood();
+                        _cacheFood = slot;
+                        _cacheFood.OnFadeFood();
+                        _cacheFood.OnSetSlot(_currentFood.GetSpriteFood);
+                    }
+                }
+                else // vi tri tro chuot da co item
+                {                    
+                    FoodSlot slotAvalable = slot.GetSlotNull;
+                    if(slotAvalable != null)
+                    {
+                        _cacheFood?.OnHideFood();
+                        _cacheFood = slotAvalable;
+                        _cacheFood.OnFadeFood();
+                        _cacheFood.OnSetSlot(_currentFood.GetSpriteFood);
+                    }
+                    else
+                    {
+                        this.OnClearCacheSlot();
+                    }
+                }
+            }
+            //else
+            //{
+            //    if(_cacheFood != null)
+            //    {
+            //        _cacheFood.OnHideFood();
+            //        _cacheFood = null;
+            //    }
+            //}
+        }
+
+        if(Input.GetMouseButtonUp(0) && _hasDrag)
+        {
+            _imgFoodDrag.gameObject.SetActive(false);
+            _currentFood.OnActiveFood(true);
+            _currentFood = null;
+            _hasDrag = false;
+
+            this.OnClearCacheSlot();
+        }
+    }
+
+    private void OnClearCacheSlot()
+    {
+        if (_cacheFood != null && _cacheFood.GetInstanceID() != _currentFood.GetInstanceID())
+        {
+            _cacheFood.OnHideFood();
+            _cacheFood = null;
+        }
+    }
+}
---
+++ Assets/Scripts/GameManagers.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GameManagers : MonoBehaviour
+{
+    [SerializeField] private int _totalFood; // tong so loai thuc an
+    [SerializeField] private int _totalGrill; // tong so bep
+    [SerializeField] private Transform _gridGrill;
+
+    private List<GrillStation> _listGrills;
+    private float _avgTray; // gia tri trung binh thuc an cho 1 dia
+    private List<Sprite> _totalSpriteFood;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+        _listGrills = Utils.GetListInChild<GrillStation>(_gridGrill);
+        Sprite[] loadedSprite = Resources.LoadAll<Sprite>("Items");
+        _totalSpriteFood = loadedSprite.ToList();
+    }
+
+    void Start()
+    {
+        OnInitLevel();
+    }
+
+    private void OnInitLevel()
+    {
+        List<Sprite> takeFood = _totalSpriteFood.OrderBy(x => Random.value).Take(_totalFood).ToList();//
+        List<Sprite> useFood = new List<Sprite>();
+
+        for(int i = 0; i < takeFood.Count; i++)
+        {
+            for (int j = 0; j < 3; j++)
+                useFood.Add(takeFood[i]);
+        }
+
+        // random, trao vi tri cua cac item
+        for(int i = 0; i < useFood.Count; i++)
+        {
+            int rand = Random.Range(i, useFood.Count);
+            (useFood[i], useFood[rand]) = (useFood[rand], useFood[i]); // ham nay laf doi vi tri i hien taij cua vong lap va vi tri random
+        }
+
+        _avgTray = Random.Range(1.5f, 2f);
+        int totalTray = Mathf.RoundToInt(useFood.Count / _avgTray);// tinh tong so dia
+
+        List<int> trayPerGrill = this.DistributeEvelyn(_totalGrill, totalTray);
+        List<int> foodPerGrill = this.DistributeEvelyn(_totalGrill, useFood.Count);
+
+        for(int i = 0; i < _listGrills.Count; i++)
+        {
+            bool activeGrill = i < _totalGrill;
+            _listGrills[i].gameObject.SetActive(activeGrill);
+
+            if (activeGrill)
+            {
+                List<Sprite> lisFood = Utils.TakeAndRemoveRandom<Sprite>(useFood, foodPerGrill[i]);
+                _listGrills[i].OnInitGrill(trayPerGrill[i], lisFood);
+            }
+        }
+
+    }
+
+    private List<int> DistributeEvelyn(int grillCount,int totalTrays)
+    {
+        List<int> result = new List<int>();
+
+        // tinh trung binh so luong dia
+        float avg = (float)totalTrays / grillCount; // 3.5
+        int low = Mathf.FloorToInt(avg); // 3
+        int high = Mathf.CeilToInt(avg); // 4
+
+        int hightCount = totalTrays - low * grillCount; // tinh so bep nhieu khay hon
+        int lowCount = grillCount - hightCount;
+
+        for (int i = 0; i < lowCount; i++)
+            result.Add(low);
+
+        for (int i = 0; i < hightCount; i++)
+            result.Add(high);
+
+        // dao vi tri
+        for(int i = 0; i < result.Count; i++)
+        {
+            int rand = Random.Range(i, result.Count);
+            (result[i], result[rand]) = (result[rand], result[i]);
+        }
+
+        return result;
+    }
+}
